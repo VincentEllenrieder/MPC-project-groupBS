d8c13e73fb744bee50c704149b2957407e11f2dc
mt-tschudin <moritz.tschudin@epfl.ch>
2025-12-12 13:25:22 +0100

    Worked on delieverable 2.1 and 3.1

--- project/LinearMPC_template/MPCControl_base.py
+++ project/LinearMPC_template/MPCControl_base.py
@@ -40,6 +40,23 @@ class MPCControl_base:
         self.nx = self.x_ids.shape[0]
         self.nu = self.u_ids.shape[0]
 
+        # Constraints
+        self.LBU = np.array([-np.deg2rad(15), -np.deg2rad(15), 40.0, -20.0])
+        self.UBU = np.array([ np.deg2rad(15),  np.deg2rad(15), 80.0,  20.0])
+
+        self.LBX = np.array([
+            -np.inf, -np.inf, -np.inf,
+            -np.deg2rad(10), -np.deg2rad(10), -np.inf,
+            -np.inf, -np.inf, -np.inf,
+            -np.inf, -np.inf, 0.0
+        ])
+        self.UBX = np.array([
+             np.inf,  np.inf,  np.inf,
+             np.deg2rad(10),  np.deg2rad(10),  np.inf,
+             np.inf,  np.inf,  np.inf,
+             np.inf,  np.inf,  np.inf
+        ])
+
         # System definition
         xids_xi, xids_xj = np.meshgrid(self.x_ids, self.x_ids)
         A_red = A[xids_xi, xids_xj].T
@@ -55,8 +72,47 @@ class MPCControl_base:
     def _setup_controller(self) -> None:
         #################################################
         # YOUR CODE HERE
+        self.x0_param = cp.Parameter(self.nx)
+        self.x = cp.Variable((self.nx, self.N+1))  # columns: x_0 ... x_N
+        self.u = cp.Variable((self.nu, self.N))    # columns: u_0 ... u_{N-1}
 
-        self.ocp = ...
+        x = self.x
+        u = self.u
+
+        Q = np.eye(self.nx)
+        R = np.eye(self.nu)
+
+        K, P, _ = dlqr(self.A, self.B, Q, R)
+
+        cost = 0
+        for k in range(self.N):
+            cost += cp.quad_form(x[:, k], Q) + cp.quad_form(u[:, k], R)
+        cost += cp.quad_form(x[:, self.N], P)
+
+        constraints = []
+        u_min_abs = self.LBU[self.u_ids]
+        u_max_abs = self.UBU[self.u_ids]
+        x_min_abs = self.LBX[self.x_ids]
+        x_max_abs = self.UBX[self.x_ids]
+
+        u_min = u_min_abs - self.us
+        u_max = u_max_abs - self.us
+        x_min = x_min_abs - self.xs
+        x_max = x_max_abs - self.xs
+
+        for k in range(self.N):
+            # System dynamics constraints
+            constraints += [x[:, k+1] == self.A @ x[:, k] + self.B @ u[:, k]]
+            # Input constraints
+            constraints += [u_min <= u[:, k], u[:, k] <= u_max]
+            # State constraints
+            constraints += [x_min <= x[:, k], x[:, k] <= x_max]
+        # Init state constraint    
+        constraints += [x[:, 0] == self.x0_param]
+        # Terminal state constraint
+        constraints += [x_min <= x[:, self.N], x[:, self.N] <= x_max]
+
+        self.ocp = cp.Problem(cp.Minimize(cost), constraints)
 
         # YOUR CODE HERE
         #################################################
@@ -74,10 +130,15 @@ class MPCControl_base:
     ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         #################################################
         # YOUR CODE HERE
+        x0_sub = x0[self.x_ids]
+        self.x0_param.value = x0_sub - self.xs
+        self.ocp.solve(solver=cp.OSQP, warm_start=True)
 
-        u0 = ...
-        x_traj = ...
-        u_traj = ...
+        u0_delta = self.u[:, 0].value
+        u0 = self.us + u0_delta
+
+        x_traj = self.x.value
+        u_traj = self.u.value
 
         # YOUR CODE HERE
         #################################################
--- project/LinearMPC_template/MPCControl_zvel.py
+++ project/LinearMPC_template/MPCControl_zvel.py
@@ -11,30 +11,6 @@ class MPCControl_zvel(MPCControl_base):
     d_estimate: np.ndarray
     d_gain: float
 
-    def _setup_controller(self) -> None:
-        #################################################
-        # YOUR CODE HERE
-
-        self.ocp = ...
-
-        # YOUR CODE HERE
-        #################################################
-
-    def get_u(
-        self, x0: np.ndarray, x_target: np.ndarray = None, u_target: np.ndarray = None
-    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-        #################################################
-        # YOUR CODE HERE
-
-        u0 = ...
-        x_traj = ...
-        u_traj = ...
-
-        # YOUR CODE HERE
-        #################################################
-
-        return u0, x_traj, u_traj
-
     def setup_estimator(self):
         # FOR PART 5 OF THE PROJECT
         ##################################################
