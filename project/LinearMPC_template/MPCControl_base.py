1b8c090206edc66c2dbcf37ad010d08afb310f0b
VincentEllenrieder <80537351+VincentEllenrieder@users.noreply.github.com>
2025-12-19 16:15:44 +0100

    3.2 Tracking work in progress for final version (no invariant set re-scaling using X_delta and U_delta)

--- project/LinearMPC_template/MPCControl_base.py
+++ project/LinearMPC_template/MPCControl_base.py
@@ -63,14 +63,14 @@ class MPCControl_base:
         # Bounds definition
         UBU = np.array([np.deg2rad(15), np.deg2rad(15), 80, 20])
         LBU = np.array([-np.deg2rad(15), -np.deg2rad(15), 40.0, -20.0])
-        UBX = np.array([50,  50,  50,
-                        np.deg2rad(10),  np.deg2rad(10),  50,
-                        50,  50,  50,
-                        50,  50, 50])
-        LBX = np.array([-50, -50, -50,
-                        -np.deg2rad(10), -np.deg2rad(10), -50,
-                        -50, -50, -50,
-                        -50, -50, 0.0])
+        UBX = np.array([np.inf,  np.inf,  np.inf,
+                        np.deg2rad(10),  np.deg2rad(10),  np.inf,
+                        np.inf,  np.inf,  np.inf,
+                        np.inf,  np.inf, np.inf])
+        LBX = np.array([-np.inf, -np.inf, -np.inf,
+                        -np.deg2rad(10), -np.deg2rad(10), -np.inf,
+                        -np.inf, -np.inf, -np.inf,
+                        -np.inf, -np.inf, 0.0])
         self.UBU = UBU[self.u_ids]
         self.LBU = LBU[self.u_ids]
         self.UBX = UBX[self.x_ids]
@@ -111,126 +111,32 @@ class MPCControl_base:
         u_diff = self.u_var - cp.reshape(self.ut_par, (self.nu, 1))
 
         # Costs (objective function)
-        self.cost = 0
+        cost = 0
         for k in range(self.N):
-            self.cost += cp.quad_form(x_diff[:,k], self.Q)
-            self.cost += cp.quad_form(u_diff[:,k], self.R)
+            cost += cp.quad_form(x_diff[:,k], self.Q)
+            cost += cp.quad_form(u_diff[:,k], self.R)
         
         # Terminal cost
         K, P, _ = dlqr(self.A, self.B, self.Q, self.R)
-        self.K = -K
-        self.cost += cp.quad_form(x_diff[:, -1], P)        
+        K = -K
+        cost += cp.quad_form(x_diff[:, -1], P)        
 
         # System (equality) constraint
-        self.base_constraints = []
-        self.base_constraints.append(self.x_var[:, 0] == self.x0_par)
-        self.base_constraints.append(self.A @ x_diff[:, :-1] + self.B @ u_diff == x_diff[:, 1:]) # x^+ - xt = A(x-xt) + B(u-ut)
+        constraints = []
+        constraints.append(self.x_var[:, 0] == self.x0_par)
+        constraints.append(self.A @ (self.x_var - cp.reshape(self.xs, (self.nx, 1)))[:, :-1] + self.B @ (self.u_var - cp.reshape(self.us, (self.nu, 1))) == (self.x_var - cp.reshape(self.xs, (self.nx, 1)))[:, 1:]) # x^+ - xs = A(x-xs) + B(u-us)
 
         # Inequality constraints
-        self.base_constraints.append(self.X.A @ self.x_var <= self.X.b.reshape(-1, 1)) # x in X for all k = 0, ..., N
+        constraints.append(self.X.A @ self.x_var <= self.X.b.reshape(-1, 1)) # x in X for all k = 0, ..., N
 
-        self.base_constraints.append(self.U.A @ self.u_var <= self.U.b.reshape(-1, 1)) # u in U for all k = 0, ..., N-1
+        constraints.append(self.U.A @ self.u_var <= self.U.b.reshape(-1, 1)) # u in U for all k = 0, ..., N-1
 
-    @staticmethod
-    def _discretize(A: np.ndarray, B: np.ndarray, Ts: float):
-        nx, nu = B.shape
-        C = np.zeros((1, nx))
-        D = np.zeros((1, nu))
-        A_discrete, B_discrete, _, _, _ = cont2discrete(system=(A, B, C, D), dt=Ts)
-        return A_discrete, B_discrete
+        # Terminal constraint
+        A_cl = self.A + self.B @ K
 
-    def get_u(
-        self, x0: np.ndarray, show_Xf: bool, x_target: np.ndarray = None, u_target: np.ndarray = None) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        KU = Polyhedron.from_Hrep(self.U.A @ K, self.U.b)
+        Omega = self.X.intersect(KU)
         
-        # Initialize QP parameters
-        self.x0_par.value = x0
-        if x_target is None:
-            self.xt_par.value = self.xs
-        else:
-            self.xt_par.value = x_target
-        if u_target is None:
-            self.ut_par.value = self.us
-        else:
-            self.ut_par.value = u_target
-
-        # Build terminal set + terminal constraints USING (x_t, u_t) IF targets have changed sice last call of get_u:
-        
-        # If the invariant set has never been constructef yet, compute it
-        if self._cached_xt is None or self._cached_ut is None or self._cached_Xf is None:
-            self.X_f = self.compute_terminal_set_about_target(self.xt_par.value, self.ut_par.value, max_iter=50)
-        else:
-            # If x_target and u_target same as previous one, don't compute the invariant set again
-            if np.allclose(self.xt_par.value, self._cached_xt, rtol=1e-10, atol=1e-10) and np.allclose(self.ut_par.value, self._cached_ut, rtol=1e-10, atol=1e-10):
-                self.X_f = self._cached_Xf
-            # If x_target or u_target changed with respect to their value the last time u_get was called, compute the invariant set again
-            else:
-                self.X_f = self.compute_terminal_set_about_target(self.xt_par.value, self.ut_par.value, max_iter=50)
-
-        # If empty terminal set -> infeasible
-        if self.X_f.Vrep.V.size == 0:
-            raise RuntimeError(
-                f"[{self.subsys_name}] Terminal set is EMPTY around current target; QP will be infeasible."
-            )
-        
-        self._cached_xt = self.xt_par.value.copy()
-        self._cached_ut = self.ut_par.value.copy()
-        self._cached_Xf = self.X_f
-
-        x_diff_N = self.x_var[:, -1] - self.xt_par  # (nx,)
-
-        terminal_constraints = []
-        terminal_constraints.append(self.X_f.A @ x_diff_N <= self.X_f.b) 
-        terminal_constraints.append(self.U.A @ (self.ut_par + self.K @ x_diff_N) <= self.U.b)
-
-        if show_Xf == True :
-            self.plot_Xf(self.xt_par.value)
-
-        # Build and solve FINAL OCP (base + terminal)
-        ocp = cp.Problem(cp.Minimize(self.cost), self.base_constraints + terminal_constraints)
-
-        ocp.solve(
-            solver=cp.PIQP,
-            warm_start=True,
-            max_iter=20000,
-            eps_abs=1e-4,
-            eps_rel=1e-4
-        )
-
-        if ocp.status not in ("optimal", "optimal_inaccurate"):
-            raise RuntimeError(f"QP problem failed: {ocp.status}")
-
-        u0 = self.u_var.value[:, 0]
-        x_traj = self.x_var.value
-        u_traj = self.u_var.value
-        # print(f"u0 for system {self.subsys_name} is {u0}\n")
-        # print(f"x_ol for system {self.subsys_name} is {x_traj}\n")
-        # print(f"u_ol for system {self.subsys_name} is {u_traj}\n")
-
-        return u0, x_traj, u_traj
-
-    def compute_terminal_set_about_target(
-        self,
-        xt: np.ndarray,
-        ut: np.ndarray,
-        max_iter: int = 50,
-    ) -> Polyhedron:
-        """
-        Compute maximal invariant set for delta dynamics:
-            x+ - xt = (A + B K) (x - xt)
-        subject to:
-            x - xt in X_delta = {x - xt : x in X}
-            K (x - xt) in U_delta = {u - ut : u in U}
-        """
-        # Build delta-sets wrt CURRENT reference (x_t, u_t)
-        # X_delta = {x-xt | x in X} -> bounds shift
-        X_delta = Polyhedron.from_bounds(self.LBX - xt, self.UBX - xt)
-        U_delta = Polyhedron.from_bounds(self.LBU - ut, self.UBU - ut)
-
-        A_cl = self.A + self.B @ self.K
-
-        KU_delta = Polyhedron.from_Hrep(U_delta.A @ self.K, U_delta.b)
-        Omega = X_delta.intersect(KU_delta)
-
         i = 0
         max_iter = 50
         while i < max_iter :
@@ -247,8 +153,61 @@ class MPCControl_base:
             Omega = Omega_new
             i += 1
 
-        return Omega
+        self.X_f = Omega
+        # If empty terminal set -> infeasible
+        if self.X_f.Vrep.V.size == 0:
+            raise RuntimeError(
+                f"[{self.subsys_name}] Terminal set is EMPTY around current target; QP will be infeasible."
+            )
+        
+        constraints.append(self.X_f.A @ x_diff[:, -1] <= self.X_f.b)
 
+        self.ocp = cp.Problem(cp.Minimize(cost), constraints)
+
+    @staticmethod
+    def _discretize(A: np.ndarray, B: np.ndarray, Ts: float):
+        nx, nu = B.shape
+        C = np.zeros((1, nx))
+        D = np.zeros((1, nu))
+        A_discrete, B_discrete, _, _, _ = cont2discrete(system=(A, B, C, D), dt=Ts)
+        return A_discrete, B_discrete
+
+    def get_u(
+        self, x0: np.ndarray, show_Xf: bool, x_target: np.ndarray = None, u_target: np.ndarray = None) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        
+        if show_Xf == True :
+            self.plot_Xf(self.xt_par.value)
+
+        # Initialize QP parameters
+        self.x0_par.value = x0
+        if x_target is None:
+            self.xt_par.value = self.xs
+        else:
+            self.xt_par.value = x_target
+        if u_target is None:
+            self.ut_par.value = self.us
+        else:
+            self.ut_par.value = u_target
+
+        self.ocp.solve(
+            solver=cp.PIQP,
+            warm_start=True,
+            max_iter=20000,
+            eps_abs=1e-4,
+            eps_rel=1e-4
+        )
+
+        if self.ocp.status not in ("optimal", "optimal_inaccurate"):
+            raise RuntimeError(f"QP problem failed: {self.ocp.status}")
+        
+        u0 = self.u_var.value[:, 0]
+        x_traj = self.x_var.value
+        u_traj = self.u_var.value
+        # print(f"u0 for system {self.subsys_name} is {u0}\n")
+        # print(f"x_ol for system {self.subsys_name} is {x_traj}\n")
+        # print(f"u_ol for system {self.subsys_name} is {u_traj}\n")
+
+        return u0, x_traj, u_traj
 
     def plot_Xf(self, x_target):
         pairs = list(combinations(range(self.nx), 2))
