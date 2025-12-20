4b350ba180fe2e08e26c4820dbef277a7a88b267
mt-tschudin <moritz.tschudin@epfl.ch>
2025-12-20 13:52:48 +0100

    Fixed 3.3 using Q = 1 for z, and new simulation duration of 30 seconds

--- project/Deliverable_3_3/MPCControl_zvel_D3_3.py
+++ project/Deliverable_3_3/MPCControl_zvel_D3_3.py
@@ -25,8 +25,8 @@ class MPCControl_zvel_tuned_final(MPCControl_base):
     vz_max = 5.0
     P_max = 80
 
-
-    Q = np.array([[20 * (1/(vz_max**2))]])
+    Q = np.array([[25 * (1/(vz_max**2))]])
+    #Q = np.array([[20 * (1/(vz_max**2))]]) didn't work!
     R = np.array([[50 * (1/(P_max**2))]])
 
 
