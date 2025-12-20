1d8b25ddbc213adf7b14442e2fab2e30d4b703b6
mt-tschudin <moritz.tschudin@epfl.ch>
2025-12-20 13:22:54 +0100

    Tuned R factor for D3.1, D3.2, D3.3 D3.1 and D3.2 work now, started integrated vincent's comments
    need to fix 3.3 now

--- project/Deliverable_3_1/MPCControl_zvel_D3_1.py
+++ project/Deliverable_3_1/MPCControl_zvel_D3_1.py
@@ -12,20 +12,20 @@ class MPCControl_zvel_tuned_default(MPCControl_base):
     u_ids: np.ndarray = np.array([2])   # Pavg
 
     vz_max = 5.0
-    dP_max = min(80 - 66.7, 66.7 - 40.0)  # m/s per control step
+    P_max = 80
 
     Q = np.array([[1/(vz_max**2)]])
-    R = np.array([[1/(dP_max**2)]])
+    R = np.array([[1/(P_max**2)]])
 
 class MPCControl_zvel_tuned_final(MPCControl_base):
     x_ids: np.ndarray = np.array([8])   # vz
     u_ids: np.ndarray = np.array([2])   # Pavg
 
     vz_max = 5.0
-    dP_max = min(80 - 66.7, 66.7 - 40.0)  # m/s per control step
+    P_max = 80
 
 
     Q = np.array([[20 * (1/(vz_max**2))]])
-    R = np.array([[(1/(dP_max**2)) / 2]])
+    R = np.array([[50 * (1/(P_max**2))]])
 
 
--- project/Deliverable_3_3/MPCControl_zvel_D3_3.py
+++ project/Deliverable_3_3/MPCControl_zvel_D3_3.py
@@ -8,25 +8,25 @@ class MPCControl_zvel(MPCControl_base):
     u_ids: np.ndarray = np.array([2])   # Pavg
     tracked_idx = 0   # only state is v_z
 
-class MPCControl_zvel_tuned_default(MPCControl_zvel):
+class MPCControl_zvel_tuned_default(MPCControl_base):
     x_ids: np.ndarray = np.array([8])   # vz
     u_ids: np.ndarray = np.array([2])   # Pavg
 
     vz_max = 5.0
-    dP_max = min(80 - 66.7, 66.7 - 40.0)  # m/s per control step
+    P_max = 80
 
     Q = np.array([[1/(vz_max**2)]])
-    R = np.array([[1/(dP_max**2)]])
+    R = np.array([[1/(P_max**2)]])
 
-class MPCControl_zvel_tuned_final(MPCControl_zvel):
+class MPCControl_zvel_tuned_final(MPCControl_base):
     x_ids: np.ndarray = np.array([8])   # vz
     u_ids: np.ndarray = np.array([2])   # Pavg
 
     vz_max = 5.0
-    dP_max = min(80 - 66.7, 66.7 - 40.0)  # m/s per control step
+    P_max = 80
 
 
     Q = np.array([[20 * (1/(vz_max**2))]])
-    R = np.array([[(1/(dP_max**2)) / 2]])
+    R = np.array([[50 * (1/(P_max**2))]])
 
 
