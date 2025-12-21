0f6ed68a124f3789a10f6de61fcfaafc6808bf1f
mt-tschudin <moritz.tschudin@epfl.ch>
2025-12-21 15:00:41 +0100

    Fixed vzmax in del 2-4 and ran again plots, del 4 is checked

--- project/Deliverable_3_1/MPCControl_zvel_D3_1.py
+++ project/Deliverable_3_1/MPCControl_zvel_D3_1.py
@@ -21,7 +21,7 @@ class MPCControl_zvel_tuned_final(MPCControl_base):
     x_ids: np.ndarray = np.array([8])   # vz
     u_ids: np.ndarray = np.array([2])   # Pavg
 
-    vz_max = 5.0
+    vz_max = 10.0
     P_max = 80
 
     Q = np.array([[80 * (1/(vz_max**2))]])
